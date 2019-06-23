114b89a6d5ac61e4f8ee73898c0cd8ba27c7e5c2
Sebastian Murgul <sebastian.murgul@waldisoft.com>
2019-06-23 22:55:52 +0200

    Updates

---
+++ precache-manifest.6d763ca889ab163f47164e90ca0a42cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fbb6911bf0c38926d0dbaa214a1abe",
+    "url": "/bark-face/index.html"
+  },
+  {
+    "revision": "a7bf75d55408b953e31c",
+    "url": "/bark-face/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "232a5299bb85df539cae",
+    "url": "/bark-face/static/js/2.f5cde3f3.chunk.js"
+  },
+  {
+    "revision": "a7bf75d55408b953e31c",
+    "url": "/bark-face/static/js/main.313c099f.chunk.js"
+  },
+  {
+    "revision": "d4e5bfbd76e4bcaa82da",
+    "url": "/bark-face/static/js/runtime~main.f771ce3a.js"
+  }
+]);
