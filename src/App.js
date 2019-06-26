e31aadc91a43bc7f1e2a71f096237deb129db20a
Sebastian Murgul <sebastian.murgul@waldisoft.com>
2019-06-26 07:11:35 +0200

    fix wholes in characters

--- src/App.js
+++ src/App.js
@@ -1,140 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Grid, Typography, Select, Button, MenuItem, TextField } from '@material-ui/core';
-import { A_1, A_2, A_ALL, A_ALTERNATIVE_1, E_1, B_1, D_1, F_1, H_1, K_1, L_1, N_1, T_1, U_1, CHARS } from './examples';
+import { Typography, Button, AppBar, Toolbar } from '@material-ui/core';
 import { withStyles } from '@material-ui/styles';
-import { parsePath, writePath } from './svg/SVGUtils';
-import { Vector2D, analyzePath, pathToPoints, smoothPath, insidePolygon, crackPoints, getSubPaths, roughenPath, stretch, rounden } from './svg/geometry';
-import { Slider } from 'material-ui-slider';
-import { resetSeed, random } from './svg/random';
+import WriteDemo from './WriteDemo';
+import SketchDemo from './SketchDemo';
+import { Route, Switch } from "react-router-dom";
 
-const styles = theme => ({
-  transformContainer: {
-    height: 700,
+const styles = () => ({
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+    height: 50,
   },
-  canvas: {
-    width: "100%",
-    height: "100%",
-  }
+  root: {
+    height: "100vh"
+  },
+  grow: {
+    flexGrow: 1,
+  },
 });
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      srcPath: A_1,
-      resPath: [],
-      circles: [],
-      lines: [],
-      age: 0,
-      play: false,
-      text: "SEBI",
-      seed: 42
-    }
   }
-
-  componentDidMount() {    
-    this.transformLetter();
-    setInterval(() => {
-      const {age, play} = this.state;
-      if(play) {
-        if(age === 100) {
-          this.setState({age: 0})
-        } else {        
-          this.setState({age: age + 1})
-        }
-        this.transformLetter();
-      }
-    }, 200)
-  }
-
-  transformLetter = () => {
-    const {age, text} = this.state;
-    let resPath = [];
-
-    for(let c of text) {
-      resetSeed(this.state.seed);
-      
-      var srcPath = parsePath(CHARS[c]);
-
-      // randomization parameters
-      let magn = new Vector2D(
-        age * 1.0 + random() * 3, 
-        age * 0.5 + random() * 2
-      );
-      let roundingMagnitude = age + random() * 3;
-
-      var circles = [];
-      var lines = [];
-
-      let { points, normals } = analyzePath(srcPath);
-
-      // rounden path
-      let newPath = rounden(srcPath, points, normals, roundingMagnitude);
-
-      // stretch
-      newPath = stretch(newPath, points, normals, magn)    
-
-      // smooth path
-      let newPathSmoothed = smoothPath(newPath, 0.15)
-
-      // roughen path
-      let roughPath = roughenPath(newPathSmoothed, age * 8/100);
-
-      // smooth roughened again
-      roughPath = smoothPath(roughPath, 0.15);
-
-      // crack points overlay
-      roughPath = crackPoints(roughPath, age);
-
-      resPath.push(writePath(roughPath));
-    }
-    
-    this.setState({
-      resPath: resPath,
-      circles: circles,
-      lines: lines
-    })
-  }
-
-  handleSelect = (e) => {
-    this.setState({
-      srcPath: e.target.value,
-    });
-    setTimeout(() => this.transformLetter(), 100);
-  }
-
   render(){
     const {classes} = this.props;
-    const {srcPath, resPath, circles, lines} = this.state;
 
-    const svgText = '<svg viewBox="1000 800 1000 1500"><path d="' + resPath + '"/><path d="' + srcPath + '" fill="#ffff00"/>  </svg>'
     return (
-      <div className="App">
-        <Typography variant="h5">Parameters:</Typography>
-        <Button onClick={() => {
-          this.setState({seed: Math.random() * 100});      
-          setTimeout(() => this.transformLetter(), 10);
-        }}>Shuffle</Button>
-        <TextField value={this.state.text} onChange={(e) => {
-          this.setState({text: e.target.value});
-          setTimeout(() => this.transformLetter(), 10);
-        }} />
-        <Button onClick={() => this.setState({play: !this.state.play})}>{this.state.play ? "Pause": "Play"}</Button>
-        <Slider value={this.state.age} onChange={(e)=>{
-          this.setState({age: e});
-          setTimeout(() => this.transformLetter(), 10);
-        }}></Slider>
-        <Typography variant="h5">Result:</Typography>
-        <Grid container className={classes.transformContainer}>
-          <Grid item xs={12}>
-            <svg viewBox={"1000 800 " + (1000 * resPath.length) + " 1500"}>
-              {resPath.map((path, index) => (
-                <path transform={"translate(" + (1000 * index) + ", 0)"} d={path}/> 
-              ))}   
-            </svg>
-          </Grid>
-        </Grid>
+      <div className="root">
+        <AppBar position="">
+          <Toolbar>
+            <a aria-label="Menu" href="/">
+              <img className={classes.menuButton} src="/examples/A_5.svg" />
+            </a>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Bark Face
+            </Typography>
+            <Button color="inherit" href="#/sketch">
+              Sketch
+            </Button>
+            <Button color="inherit" href="#/write">
+              Write
+            </Button>
+          </Toolbar>
+        </AppBar>  
+        <Switch>
+          <Route path="/sketch" component={SketchDemo} />
+          <Route component={WriteDemo} />        
+        </Switch>  
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#0673C1',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#182A42',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    error: red,
+    // Used by `getContrastText()` to maximize the contrast between the background and
+    // the text.
+    contrastThreshold: 3,
+    // Used to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+ReactDOM.render(
+  <HashRouter basename={baseUrl}>
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  </HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
